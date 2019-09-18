428670e839eaf460ad253e37921dcf60d127ca7d
Matthias Hosp <matthiashosp@Matthiass-MacBook-Pro.local>
2019-09-18 13:24:22 +0200

    added canvas that changes color by pressing space bar

---
+++ HelloWorld/Assets/RandomColor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomColor : MonoBehaviour {
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey(KeyCode.Space)) {
+            this.GetComponent<Renderer>().material.color = Color.HSVToRGB(Random.Range(0f, 1f),Random.Range(0f, 1f),Random.Range(0f, 1f));
+        }
+    }
+}
