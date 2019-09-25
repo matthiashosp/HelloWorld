98cfd5858a20b826b273df96a817208308a8ece4
Matthias Hosp <matthiashosp@Matthiass-MacBook-Pro.local>
2019-09-25 11:55:09 +0200

    accidently deleted Script and added it again

---
+++ HelloWorld/Assets/SceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour {
+
+    public void LoadNextScene()
+    {
+        Application.LoadLevel("scene 2");
+    }
+
+    public void LoadPreviousScene()
+    {
+        Application.LoadLevel("scene 1");
+    }
+}
